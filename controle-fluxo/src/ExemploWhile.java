e44f278cf3dc0a3945870d7acf19f4bf3d0aa794
David Anderson <davidmaceio@hotmail.com>
2023-09-27 07:37:13 -0300

    Atualizando diretório remoto VSCODE

---
+++ controle-fluxo/src/ExemploWhile.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+
+        while (mesada > 0){
+            double valorDoce = valorAleatorio();
+            if (valorDoce > mesada)
+            valorDoce = mesada;
+                System.out.println("Doce de valor " + valorDoce + " adicionado ao carrinho;");
+            mesada = mesada - valorDoce;
+
+        }
+        System.out.println("Mesada" + mesada);
+        System.out.println("Joãozinho gastou toda sua mesada em doces");
+
+    }
+
+    private static double valorAleatorio(){
+            return ThreadLocalRandom.current().nextDouble(2,8);
+    }
+}
